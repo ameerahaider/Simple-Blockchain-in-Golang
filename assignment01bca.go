7897f5ee7d25216655da5a48bcc9d4e2e484a342
Ameera <ameera.rizvi@hotmail.com>
2023-09-17 13:22:10 +0500

    Final Commit

--- assignment01bca.go
+++ assignment01bca.go
@@ -45,7 +45,7 @@ func (bc *Blockchain) NewBlock(transaction string, nonce int, previousHash strin
 }
 
 func PrintBlock(b *Block) {
-	fmt.Printf("%s Block %s\n", strings.Repeat("=", 25), strings.Repeat("=", 25))
+	fmt.Printf("%s Block %s\n", strings.Repeat("=", 30), strings.Repeat("=", 30))
 	fmt.Printf("Transaction: %s\n", b.transaction)
 	fmt.Printf("Nonce: %d\n", b.nonce)
 	fmt.Printf("Previous Hash: %s\n", b.prevHash)
@@ -82,33 +82,4 @@ func VerifyChain(bc *Blockchain, LastHash string) {
 	}
 
 	fmt.Println("\nChain Verified: All blocks are connected and valid.")
-}
-
-func main() {
-
-	//Make Blockchain
-	blockchain := new(Blockchain)
-	b1 := blockchain.NewBlock("Transaction 1", 1, "")
-	b2 := blockchain.NewBlock("Transaction 2", 2, CalculateHash(b1))
-	b3 := blockchain.NewBlock("Transaction 3", 3, CalculateHash(b2))
-	b4 := blockchain.NewBlock("Transaction 4", 4, CalculateHash(b3))
-	b5 := blockchain.NewBlock("Transaction 5", 5, CalculateHash(b4))
-	b6 := blockchain.NewBlock("Transaction 6", 6, CalculateHash(b5))
-	b7 := blockchain.NewBlock("", 7, CalculateHash(b6))
-	LastHash := b7.prevHash //Store Last Hash Only
-
-	//Print Chain
-	ListBlocks(blockchain)
-
-	//Verify Chain
-	VerifyChain(blockchain, LastHash)
-
-	//Tamper Chain
-	ChangeBlock(b2, "Transaction 2 Updated")
-
-	//Print Tampered Chain
-	ListBlocks(blockchain)
-
-	//Verify Tampered Chain
-	VerifyChain(blockchain, LastHash)
 }
